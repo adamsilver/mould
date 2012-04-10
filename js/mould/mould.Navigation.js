60370b2bda81f4e0e3583af039d2fdb631e3853a
Adam Silver <adambsilver@gmail.com>
2012-04-10 13:10:47 +0100

    fixes

--- js/mould/mould.Navigation.js
+++ js/mould/mould.Navigation.js
@@ -5,7 +5,8 @@ if(jessie.attachWindowListener &&
 	jessie.removeClass) {
 
 	mould.Navigation = function(menuContainer, headerContainer) {
-		this.showingMenu = true;
+		this.compactModeEnabled = false;
+		this.showingMenu = false;
 		this.menuContainer = menuContainer;
 		this.headerContainer = headerContainer;
 		this.windowResizeListener = jessie.bind(this.handleWindow_onResize, this);
@@ -16,24 +17,23 @@ if(jessie.attachWindowListener &&
 	mould.Navigation.prototype.createButton = function() {
 		this.button = document.createElement("a");
 		this.button.className = "navigationButton";
-		this.button.innerHTML = "Navigation";
 		this.button.href = "#";		
 		//this.headerContainer.appendChild(this.button);
 		this.headerContainer.insertBefore(this.button, this.headerContainer.childNodes[0]);
 		jessie.attachBoundListener(this.button, "click", this.handleButton_onClick, this);
 	};
 	mould.Navigation.prototype.enableCompactMode = function() {
-		//this.showButton();
-		this.hideMenu();
-		this.showingMenu = false;
+		if(this.compactModeEnabled == false) {
+			this.hideMenu();
+			this.showingMenu = false;
+			this.compactModeEnabled = true;
+		}
 	};
 	mould.Navigation.prototype.handleButton_onClick = function(e) {
 		e.preventDefault();
-
 		// if this button has been clicked we can definitely assume that we are in compact mode
-		var buttonPosition = jessie.getElementPositionStyles(e.target);
+		//var buttonPosition = jessie.getElementPositionStyles(e.target);
 		//jessie.setPositionStyles();
-
 		if(this.showingMenu) {
 			this.hideMenu();
 		}
@@ -48,25 +48,31 @@ if(jessie.attachWindowListener &&
 
 	};
 	mould.Navigation.prototype.showMenu = function() {
+		//console.log("showMenu");
 		jessie.removeClass(this.menuContainer, "hide");
 		jessie.addClass(this.button, "activated");
 		this.showingMenu = true;
+		
 	};
 	mould.Navigation.prototype.hideMenu = function() {
+		//console.log("hideMenu");
 		jessie.addClass(this.menuContainer, "hide");
 		jessie.removeClass(this.button, "activated");
 		this.showingMenu = false;
 	};
 	mould.Navigation.prototype.disableCompactMode = function() {
-		this.hideButton();
-		this.showMenu();
+		if(this.compactModeEnabled == true) {
+			this.hideButton();
+			this.showMenu();
+			this.compactModeEnabled = false;
+		}
 	};
 	mould.Navigation.prototype.handleWindow_onResize = function(e) {
 		this.check();
 	};
 	mould.Navigation.prototype.check = function() {
 		var width = document.documentElement.clientWidth;
-		if(width < 320) {
+		if(width <= 320) {
 			this.enableCompactMode();
 		}
 		else {
--- js/mould/mould.Ribbon.js
+++ js/mould/mould.Ribbon.js
@@ -12,21 +12,21 @@ if(jessie.attachBoundListener) {
 	mould.Ribbon.prototype.createButton = function() {
 		this.button = document.createElement("a");
 		this.button.className = "ribbonButton";
-		this.button.innerHTML = "Show Ribbon";
+		this.button.innerHTML = "<span>Show Ribbon</span>";
 		this.button.href="#";
-
+		this.text = this.button.childNodes[0];
 		jessie.attachBoundListener(this.button, "click", this.handleButton_onClick, this);
 		this.headerContainer.appendChild(this.button);
 	};
 	mould.Ribbon.prototype.showRibbon = function() {
 		this.ribbonContainer.style.display = "block";
 		this.showingRibbon = true;
-		this.button.innerHTML = "Hide Ribbon";
+		this.text.innerHTML = "Hide Ribbon";
 	};
 	mould.Ribbon.prototype.hideRibbon = function() {
 		this.ribbonContainer.style.display = "none";
 		this.showingRibbon = false;
-		this.button.innerHTML = "Show Ribbon";
+		this.text.innerHTML = "Show Ribbon";
 	};
 	mould.Ribbon.prototype.handleButton_onClick = function(e) {
 		e.preventDefault();
